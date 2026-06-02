c3adf90d5303a279f6df21cbc28920b10aaa1a11
YT <yaroslav.teper@gmail.com>
2026-06-01 23:03:09 -0700

    Initial Commit

---
+++ PaNIN detector/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PaNIN detector
+//
+//  Created by yaroslav teper on 6/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PaNIN detector/PaNIN_detectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaNIN_detectorApp.swift
+//  PaNIN detector
+//
+//  Created by yaroslav teper on 6/1/26.
+//
+
+import SwiftUI
+
+@main
+struct PaNIN_detectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
